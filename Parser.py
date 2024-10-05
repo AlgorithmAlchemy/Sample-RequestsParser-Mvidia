2532c28768e043b968ac577ba71dc161464e97e9
AlgorithmAlchemy <35882809+AlgorithmAlchemy@users.noreply.github.com>
2024-10-06 02:11:17 +0300

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -16,42 +16,52 @@ logging.basicConfig(level=logging.FATAL)
 
 db = sqlite3.connect('bd.sqlite')
 connect = db.cursor()
-connect.execute('''CREATE TABLE IF NOT EXISTS xboxe_id (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  item_name TEXT,
-  item_id TEXT,
-  UNIQUE (item_id)
-)''')
 
-connect.execute('''CREATE TABLE IF NOT EXISTS playstation_id_ps5 (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  item_name TEXT,
-  item_id TEXT,
-  UNIQUE (item_id)
-)''')
+# Функция для создания таблиц
+def create_tables(cursor):
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS xboxe_id (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        item_name TEXT,
+        item_id TEXT,
+        UNIQUE (item_id)
+    )''')
+    
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS playstation_id_ps5 (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        item_name TEXT,
+        item_id TEXT,
+        UNIQUE (item_id)
+    )''')
 
-connect.execute('''CREATE TABLE IF NOT EXISTS playstation_id_ps4 (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  item_name TEXT,
-  item_id TEXT,
-  UNIQUE (item_id)
-)''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS playstation_id_ps4 (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        item_name TEXT,
+        item_id TEXT,
+        UNIQUE (item_id)
+    )''')
 
-connect.execute('''CREATE TABLE IF NOT EXISTS nintendo_id (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  item_name TEXT,
-  item_id TEXT,
-  UNIQUE (item_id)
-)''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS nintendo_id (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        item_name TEXT,
+        item_id TEXT,
+        UNIQUE (item_id)
+    )''')
 
-connect.execute('''CREATE TABLE IF NOT EXISTS items (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  item_name TEXT,
-  item_id TEXT,
-  tovar_price TEXT,
-  tovar_category TEXT,
-  UNIQUE (item_id)
-)''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS items (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        item_name TEXT,
+        item_id TEXT,
+        tovar_price TEXT,
+        tovar_category TEXT,
+        UNIQUE (item_id)
+    )''')
+
+create_tables(cursor)
 
 cookies = {
     'CACHE_INDICATOR': 'false',
@@ -149,7 +159,6 @@ headers = {
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Dest': 'empty',
     'Referer': 'https://www.mvideo.ru/playstation-4327/ps5-igry-22780',
-    # 'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'en-US,en;q=0.9',
     # Requests sorts cookies= alphabetically
     # 'Cookie': 'CACHE_INDICATOR=false; COMPARISON_INDICATOR=false; HINTS_FIO_COOKIE_NAME=2; MAIN_PAGE_VARIATION_1=2; MVID_2_exp_in_1=2; MVID_AB_SERVICES_DESCRIPTION=var4; MVID_ADDRESS_COMMENT_AB_TEST=2; MVID_BLACK_FRIDAY_ENABLED=true; MVID_CALC_BONUS_RUBLES_PROFIT=false; MVID_CART_MULTI_DELETE=false; MVID_CATALOG_STATE=1; MVID_CITY_ID=CityCZ_975; MVID_FILTER_CODES=true; MVID_FILTER_TOOLTIP=1; MVID_FLOCKTORY_ON=true; MVID_GEOLOCATION_NEEDED=true; MVID_GET_LOCATION_BY_DADATA=DaData; MVID_GIFT_KIT=true; MVID_GUEST_ID=20867510091; MVID_IS_NEW_BR_WIDGET=true; MVID_KLADR_ID=7700000000000; MVID_LAYOUT_TYPE=1; MVID_LP_SOLD_VARIANTS=0; MVID_NEW_ACCESSORY=true; MVID_NEW_DESKTOP_FILTERS=true; MVID_NEW_LK=true; MVID_NEW_LK_CHECK_CAPTCHA=true; MVID_NEW_LK_LOGIN=true; MVID_NEW_LK_MENU_BUTTON=true; MVID_NEW_LK_OTP_TIMER=true; MVID_NEW_MBONUS_BLOCK=true; MVID_NEW_SUGGESTIONS=true; MVID_PDP_MAP_DEFAULT=1; MVID_PROMO_CATALOG_ON=true; MVID_REGION_ID=1; MVID_REGION_SHOP=S002; MVID_SERVICES=111; MVID_SERVICES_MINI_BLOCK=var2; MVID_TAXI_DELIVERY_INTERVALS_VIEW=old; MVID_TIMEZONE_OFFSET=3; MVID_WEBP_ENABLED=true; NEED_REQUIRE_APPLY_DISCOUNT=true; PICKUP_SEAMLESS_AB_TEST=2; PRESELECT_COURIER_DELIVERY_FOR_KBT=true; PROMOLISTING_WITHOUT_STOCK_AB_TEST=2; searchType2=2; popmechanic_sbjs_migrations=popmechanic_1418474375998%3D1%7C%7C%7C1471519752600%3D1%7C%7C%7C1471519752605%3D1; _gid=GA1.2.529201000.1655041498; _ym_uid=1655041498167988451; _ym_d=1655041498; st_uid=825b7d5ee39c6bbbbfd256545a2ea1cc; tmr_lvid=72ec0c03a55fa32f34f772c41eb0f420; tmr_lvidTS=1655041498610; advcake_track_id=7dac8892-29f9-4099-9131-71f2bdd7d472; advcake_session_id=f11f66c1-eb0e-d5f8-110e-7a6c4fac0c95; gdeslon.ru.__arc_domain=gdeslon.ru; gdeslon.ru.user_id=a20a3d27-1187-4339-894d-d392de61e8f3; _ym_isad=2; afUserId=c77ae91f-b332-4397-8027-94726b809298-p; flocktory-uuid=d8c1e6fa-ad4b-4b7b-8845-56c7377b6714-4; AF_SYNC=1655041499962; adrdel=1; adrcid=A5hrWSi919zaE4KM6YUC5UA; uxs_uid=db3eabc0-ea55-11ec-be9a-0120f2de5bab; flacktory=no; MVID_ENVCLOUD=primary; __lhash_=fe64d44a4010d930ed63e4ca7cf67e50; SMSError=; authError=; _dc_gtm_UA-1873769-1=1; _dc_gtm_UA-1873769-37=1; mindboxDeviceUUID=7caa7169-b131-45dc-a220-949a5e206e90; directCrm-session=%7B%22deviceGuid%22%3A%227caa7169-b131-45dc-a220-949a5e206e90%22%7D; _ga_CFMZTSS5FM=GS1.1.1655044522.2.1.1655045526.0; _ga_BNX5WPP3YK=GS1.1.1655044522.2.1.1655045526.6; _ga=GA1.2.86733540.1655041498; tmr_detect=0%7C1655045529970; tmr_reqNum=57; JSESSIONID=Dhf4vl9htkTyTk3jc5xWLFmhJTY4L2z3dQcB1vMMmx1LnCgTvMys!-1855659811; bIPs=-1707567431; ADRUM_BT=R:53|g:718f0ee4-2152-4f75-8fd6-e76cd009bb12879945',
@@ -212,32 +221,6 @@ def get_games_1c():
         return games  # возвращаем словарь
 
 
-"""
-def get_games_1c():
-    while True:
-        try:
-            games = []
-            r = requests.get('ТУТ ПОЛУЧАЛ ЭТОТ СПИСОК КОТОРЫЙ Я ТЕБЕ СКИНУЛ')
-
-            products = r.json()['products']
-
-            for product in products:
-                game = {}
-                game['name'] = product['name']
-                game['price'] = product['priceCache']
-
-                for platform in platforms:
-                    if platform in game['name']:
-                        games.append(game)
-                        break
-
-            return games
-        except:
-            print('reconnecting')
-            time.sleep(5)
-            continue
-"""
-
 
 def sort_data():
     for i in range(len(end_data) - 1):
@@ -266,19 +249,16 @@ def create_exel():
                 .replace("Xbox игра ", "").replace("Xbox One игра ", "").strip()
 
 
-            # parse_full_list.append({"parse_original_name": parse_original_name, "name": parse_name, "type": game_type,
-            #                         "price": parse_price})
+
             parse_full_list[parse_name] = {"parse_original_name": parse_name, "type": game_type,
                                            "price": parse_price}
-            # parse_full_list[count] = {"parse_original_name": parse_original_name, "name": parse_name, "type": game_type,
-            #                         "price": parse_price}
 
             count += 1
-            # parse_list.append(parse_name)
+
     high_key = list(parse_full_list.keys())
     high_key = high_key[-1]
 
-    json_game = get_games_1c()  # выгружаем наш жсон
+    json_game = get_games_1c()  # выгружаем наш json
     json_double = []
     count = 0
     for jsg in json_game:
@@ -351,9 +331,8 @@ def create_exel():
 
                     matches.append([json_original_name, int(json_price) - int(parse_full_list[match]['price'])])
 
-                # print(parse_full_list[match]['price'])
         count += 1
-        # matches = [match for match in parse_full_list if json_game_name in match]
+
         if not matches:
             continue
         if len(matches) >= 2:  # убрали совпадения с более чем 2 элементам исписка
@@ -363,9 +342,9 @@ def create_exel():
         else:
             json_double.append(matches)
 
-        # print(matches)
+
         strin = str(matches[0])
-        # end_list.append(matches)
+
 
 
     sort_data()
